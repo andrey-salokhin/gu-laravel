fb3ae721a7f413065080286c7695990dd31ab5f2
Andrey Salokhin <andrey.salokhin@gmail.com>
2020-11-21 22:06:26 +0600

    #homework lesson 7  added russian language  installed laravel/lang  added errors block to category and news views  added validation for adding and editing forms  added couple of tests

---
+++ app/Http/Requests/NewsUpdate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewsUpdate extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'       => ['required', 'string', 'min:3', 'max:150'],
+            'author'      => ['required', 'string', 'max:15'],
+            'description' => ['sometimes']
+        ];;
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'заголовок',
+            'description' => 'описание',
+            'author' => 'автор'
+        ];
+    }
+}
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -14,9 +14,21 @@
             @csrf
             <label for="title" class="col-form-label">Заголовок</label>
             <input id = "title" type="text" name="title" value="{{ old('title') }}" class="form-control">
+            @error('title') <div class="alert alert-danger">
+                @foreach($errors->get('title') as $error)
+                    {{ $error }}
+                @endforeach
+            </div>
+            @enderror
             <br>
             <label for="description" class="col-form-label">Описание новости</label>
             <textarea id="description" name="description" class="form-control">{{ old('description') }}</textarea>
+            @error('description') <div class="alert alert-danger">
+                @foreach($errors->get('description') as $error)
+                    {{ $error }}
+                @endforeach
+            </div>
+            @enderror
             <br>
             <input type="submit" value="Отправить" class="btn-success">
         </form>
